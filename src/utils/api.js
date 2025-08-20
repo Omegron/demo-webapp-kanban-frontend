66128de78510ce8e62f24247c35c628631d8bb96
Omegron <ministeper@gmail.com>
2025-08-20 18:25:31 +0300

    Add link with backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5184',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5184',
   headers: { 'Content-Type': 'application/json' },
 });
 
